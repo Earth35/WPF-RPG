6da68d106856acae07a30200863097b34555f583
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2018-02-27 12:33:41 +0100

    Add GameItem and Weapon classes

---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameItem
+    {
+        int ItemTypeID { get; set; }
+        string Name { get; set; }
+        int Price { get; set; }
+
+        public GameItem (int itemTypeID, string name, int price)
+        {
+            ItemTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        int MinimumDamage { get; set; }
+        int MaximumDamage { get; set; }
+
+        Weapon (int itemTypeID, string name, int price, int minimumDamage, int maximumDamage) : base (itemTypeID, name, price)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+    }
+}
